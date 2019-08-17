757ba36696fb9f4eb90d88dcbdc8def5b479d918
aarenzana <alex.arenzana@gmail.com>
2019-08-17 11:20:20 -0500

    test

--- index.php
+++ index.php
@@ -104,6 +104,8 @@
 						<div class="card-body">
 								<!--Title-->
 								<h4 class="card-title">Card title</h4>
+								
+								<!-- this is a new comment -->
 
 								<!--Text-->
 								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
